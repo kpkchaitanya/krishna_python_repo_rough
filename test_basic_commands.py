76e04d7b1862a1b4897ec53cc81e4e71b573d03a
kpkchaitanya <66326981+kpkchaitanya@users.noreply.github.com>
2025-09-05 12:06:40 +0000

    Day 1_Initial Code_To Get Started

---
+++ test_basic_commands.py
@@ -0,0 +1,13 @@
+# bikes = ['trek', 'redline', 'giant']
+# print(bikes)
+
+# # squares = []
+# # for x in range(1, 11):
+# #     squares.append(x**2)
+
+# squares = [x**2 for x in range(1, 11)]
+# print(squares)
+
+# # name = input('What is your name?')
+# # print('Your name is '+name)
+
